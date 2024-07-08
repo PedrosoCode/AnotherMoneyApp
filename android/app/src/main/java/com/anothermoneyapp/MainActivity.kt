94b37f4b9292eb8b11ff52261b875acce7cc414d
Gabriel <Gabrielpedrofc@gmail.com>
2024-07-07 22:32:27 -0300

    rename

--- android/app/src/main/java/com/anothermoneyapp/MainActivity.kt
+++ android/app/src/main/java/com/anothermoneyapp/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "AnotherMoneyApp"
+  override fun getMainComponentName(): String = "upKeepify"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- ios/upKeepifyTests/upKeepifyTests.m
+++ ios/upKeepifyTests/upKeepifyTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AnotherMoneyAppTests : XCTestCase
+@interface upKeepifyTests : XCTestCase
 
 @end
 
-@implementation AnotherMoneyAppTests
+@implementation upKeepifyTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
