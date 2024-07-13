d0e4622befa1bf594f019f34fce1b84621702f70
Gabriel <Gabrielpedrofc@gmail.com>
2024-07-12 23:40:36 -0300

    add drawer menu

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StatusBar, useColorScheme, ActivityIndicator, View } from 'react-native';
+import { StatusBar, useColorScheme } from 'react-native';
 import SQLite from 'react-native-sqlite-storage';
 
 import HomeScreen from './telas/HomeScreen';
@@ -10,6 +11,7 @@ import CadastroUsuarioScreen from './telas/CadastroUsuarioScreen';
 import UsuariosScreen from './telas/UsuariosScreen';
 import PingPongScreen from './telas/PingPongScreen';
 import CadastroMaquinaScreen from './telas/CadastroMaquinaScreen';
+
 import ListagemMaquinasScreen from './telas/ListagemMaquinasScreen';
 import DetalhesMaquinaScreen from './telas/DetalhesMaquinaScreen';
 
@@ -19,7 +21,24 @@ import { openDatabase } from './db/db';
 SQLite.DEBUG(true);
 SQLite.enablePromise(false);
 
+const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
+
+const HomeStack = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Home" component={HomeScreen} />
+    <Stack.Screen name="CadastroUsuario" component={CadastroUsuarioScreen} />
+    <Stack.Screen name="About" component={AboutScreen} />
+    <Stack.Screen name="Usuarios" component={UsuariosScreen} />
+    <Stack.Screen name="PingPong" component={PingPongScreen} />
+    <Stack.Screen name="CadastroMaquina" component={CadastroMaquinaScreen} />
+    <Stack.Screen name="ListagemMaquinas" component={ListagemMaquinasScreen} />
+    <Stack.Screen name="DetalhesMaquina" component={DetalhesMaquinaScreen} />
+  </Stack.Navigator>
+);
+
+//TODO - Criar tela de cadastro de Status, cada status deve ter uma cor, e as máquinas na tela de listagem devem ter o fundo da lista de acordo 
+         //com a cor escolhida no color picker 
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -40,16 +59,15 @@ const App = () => {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="CadastroUsuario" component={CadastroUsuarioScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="About" component={AboutScreen} />
-        <Stack.Screen name="Usuarios" component={UsuariosScreen} /> 
-        <Stack.Screen name="PingPong" component={PingPongScreen} />
-        <Stack.Screen name="CadastroMaquina" component={CadastroMaquinaScreen} />
-        <Stack.Screen name="ListagemMaquinas" component={ListagemMaquinasScreen} />
-        <Stack.Screen name="DetalhesMaquina" component={DetalhesMaquinaScreen} />
-      </Stack.Navigator>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeStack} />
+        <Drawer.Screen name="About" component={AboutScreen} />
+        <Drawer.Screen name="CadastroUsuario" component={CadastroUsuarioScreen} />
+        <Drawer.Screen name="Usuarios" component={UsuariosScreen} />
+        <Drawer.Screen name="PingPong" component={PingPongScreen} />
+        <Drawer.Screen name="CadastroMaquina" component={CadastroMaquinaScreen} />
+        <Drawer.Screen name="ListagemMaquinas" component={ListagemMaquinasScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 };
